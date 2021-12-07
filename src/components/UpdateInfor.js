706308509152fe3090f5820e7eb266e4808a49d1
19020052 <19020052@vnu.edu.vn>
2021-12-07 15:56:10 +0700

    .

--- src/components/UpdateInfor.js
+++ src/components/UpdateInfor.js
@@ -12,12 +12,12 @@ import NightsStayIcon from '@mui/icons-material/NightsStay';
 import { useParams } from 'react-router-dom';
 
 
-const paperStyle = { padding: 20, height: '130vh', width: '800px', margin: "20px 20px" }
+const paperStyle = { padding: 20, width: '800px', margin: "20px 20px" }
 
 const renderField = ({ type, lable, input }) => (
     <span className="input-row">
         <lable>{lable}</lable>
-        <input {...input} type={type} readOnly />
+        <input {...input} type={type} />
     </span >
 );
 
@@ -31,14 +31,14 @@ const renderCheck = ({ type, lable, input }) => (
 const renderOther = ({ type, lable, input }) => (
     <span className="other">
         <lable>{lable}</lable>
-        <input {...input} type={type} placeholder="Other" readOnly />
+        <input {...input} type={type} placeholder="Other" />
     </span >
 );
 
-async function submitToServer(data, id) {
+async function submitToServer(data) {
 
     try {
-        let response = await fetch('http://localhost:8000/parks/' + id, {
+        let response = await fetch('http://localhost:8000/parks/', {
             method: 'POST',
             headers: { "Content-type": "application/json" },
             body: JSON.stringify(data)
@@ -54,10 +54,10 @@ async function submitToServer(data, id) {
 
 
 
-const submit = ({ name = '', total_space = '', location = '', price = '', hasCamera = '', hasRoof = '', allowOvernight = '', allowBooking = '', description = '' }, id) => {
+const submit = ({ name = '', total_space = '', location = '', price = '', hasCamera = '', hasRoof = '', allowOvernight = '', allowBooking = '', description = '' }) => {
 
 
-    submitToServer({ name, total_space, location, price, hasCamera, hasRoof, allowOvernight, allowBooking, description }, id)
+    submitToServer({ name, total_space, location, price, hasCamera, hasRoof, allowOvernight, allowBooking, description })
         .then(data => {
             console.log(data);
             window.location.href = '/';
@@ -88,35 +88,35 @@ const ContactFormFunc = ({ handleSubmit }) => {
             {park && (
                 <Paper className="form-review" elevation={10} style={paperStyle} >
                     <Grid align='center' style={{ color: "purple" }}>
-                        <h1>Thông tin bãi đỗ {park.name}</h1>
+                        <h1>Chỉnh sửa thông tin bãi đỗ {park.name}</h1>
                     </Grid>
 
-                    <Field name="name" lable='Name' component={renderField} type="text" format={value => value === "a" ? "I'm disabled" : value} />
+                    <Field name="name" lable='Name' component={renderField} type="text" format={value => value === " " ? `${park.name}` : value} />
                     <br />
 
-                    <Field name="image_url" lable='Image' component={renderField} type="text" format={value => value = `${park.image_url}`} />
+                    <Field name="image_url" lable='Image' component={renderField} type="text" format={value => value === " " ? `${park.image_url}` : value} />
                     <br />
 
-                    <Field name="location" lable='Location' component={renderField} type="text" format={value => value = `${park.location}`} />
+                    <Field name="location" lable='Location' component={renderField} type="text" format={value => value === " " ? `${park.location}` : value} />
                     <br />
 
-                    <Field name="price" lable='Price' component={renderField} type="text" format={value => value = `${park.price}`} />
+                    <Field name="price" lable='Price' component={renderField} type="text" format={value => value === " " ? `${park.price}` : value} />
                     <br />
-                    <Field name="total_space" lable='Total space' component={renderField} type="text" format={value => value = `${park.total_space}`} />
+                    <Field name="total_space" lable='Total space' component={renderField} type="text" format={value => value === " " ? `${park.total_space}` : value} />
                     <br />
-                    <Field name="hasCamera" lable='HasCamera' component={renderCheck} type="checkbox" format={value => value = `${park.hasCamera}`} />
+                    <Field name="hasCamera" lable='HasCamera' component={renderCheck} type="checkbox" format={value => value === " " ? `${park.hasCamera}` : value} />
                     <VideoCameraBackIcon style={{ top: "200px" }} />
                     <br />
-                    <Field name="hasRoof" lable='HasRoof' component={renderCheck} type="checkbox" format={value => value = `${park.hasRoof}`} />
+                    <Field name="hasRoof" lable='HasRoof' component={renderCheck} type="checkbox" format={value => value === " " ? `${park.hasRoof}` : value} />
                     <BeachAccessIcon />
                     <br />
-                    <Field name="allowBooking" lable='AllowBooking' component={renderCheck} type="checkbox" format={value => value = `${park.allowBooking}`} />
+                    <Field name="allowBooking" lable='AllowBooking' component={renderCheck} type="checkbox" format={value => value === " " ? `${park.allowBooking}` : value} />
                     <BookIcon />
                     <br />
-                    <Field name="allowOvernight" lable='AllowOvernight' component={renderCheck} type="checkbox" format={value => value = `${park.allowOvernight}`} />
+                    <Field name="allowOvernight" lable='AllowOvernight' component={renderCheck} type="checkbox" format={value => value === " " ? `${park.allowOvernight}` : value} />
                     <NightsStayIcon />
                     <br />
-                    <Field name="description" lable='Description' component={renderOther} type="text" format={value => value = `${park.description}`} />
+                    <Field name="description" lable='Description' component={renderOther} type="text" format={value => value === " " ? `${park.description}` : value} />
                     <br /><br />
                     <label for="submit-form" tabindex="0" style={{ color: "white" }}>Submit</label>
                     <input type="submit" href="\" />
@@ -129,6 +129,7 @@ const ContactFormFunc = ({ handleSubmit }) => {
 // Decorate the form component
 const UpdateInfor = reduxForm({
     form: 'InforWatching', // a unique name for this form
+    initialValues: { name: " ", image_url: " ", location: " ", price: " ", total_space: " ", hasCamera: " ", hasRoof: " ", allowBooking: " ", allowOvernight: " ", description: " " }
 })(ContactFormFunc);
 
 export default UpdateInfor;
